4e2828e68542ef0e7338a5f930f0dadbef5b3c0a
Eliott133 <mauberteliott9@gmail.com>
2022-11-23 10:06:33 +0100

    ajout du php de raphaël

--- connexionPage/checkConnexion.php
+++ connexionPage/checkConnexion.php
@@ -1,53 +1,53 @@
 <?php
 
-    include '../Utilitaire/CheckInjectionSQL.php';
-    include '../Utilitaire/ConnexionBDD.php';
+include 'Utilitaire/CheckInjectionSQL.php';
+include 'Utilitaire/ConnexionBDD.php';
 
 
 if (isset($_POST['Connexion'])) {
 
-  $mail = $_POST['mail'];
-  $mdp = $_POST['mdp'];
+$mail = $_POST['mail'];
+$mdp = $_POST['mdp'];
 
-  $injectionSQL=CheckInjectionSQL($mail);
+$injectionSQL=CheckInjectionSQL($mail);
 
-  if (!$injectionSQL) {
+if (!$injectionSQL) {
 
-    $sql = "SELECT * FROM CONNEXION WHERE mailConnexion ='" . $mail . "' AND MDPConnexion IS NOT NULL;";
-    $rs = $bdd->query($sql);
-    $lignes = $rs->fetchAll();
-    if (count($lignes == 1)) {
-  foreach($lignes as $key => $value) {
+$sql = "SELECT * FROM CONNEXION WHERE mailConnexion ='" . $mail . "' AND MDPConnexion IS NOT NULL;";
+$rs = $bdd->query($sql);
+$lignes = $rs->fetchAll();
 
-    $mailBdd = $value['mailConnexion'];
-    $hashBdd = $value['MDPConnexion'];
-    $temporaireBdd = $value['temporaireConnexion'];
+foreach($lignes as $key => $value) {
+
+$mailBdd = $value['mailConnexion'];
+$hashBdd = $value['MDPConnexion'];
+$temporaireBdd = $value['temporaireConnexion'];
+}
+
+
+  if (password_verify($mdp, $hashBdd)) {
+
+    if ($temporaireBdd == 'oui') {
+
+
+      header('Location: CreationMDP.php?mail='.$mail);
+
+
+
+    }else{
+
+      //TODO : redirection vers la page qu'il faut
+
+      echo "<p id='message'>PAGE A DEFINIR</p>";
     }
 
+  }else{
 
-      if (password_verify($mdp, $hashBdd)) {
+      echo "<p id='message'>MDP incorrecte</p>";
 
-        if ($temporaireBdd == 'oui') {
-
-
-          header('Location: CreationMDP.php?mail='.$mail);
-
-
-
-        }else{
-
-          //TODO : redirection vers la page qu'il faut
-
-          echo "<p id='message'>PAGE A DEFINIR</p>";
-        }
-
-      }else{
-
-          echo "<p id='message'>MDP incorrecte</p>";
-
-      }
-    }
   }
-  }
+}
 
-?>
+}	
+
+ ?>
